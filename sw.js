9cd738695cbebce362393994c2fb68a29eef9458
karthikcd7 <karthikcd7@gmail.com>
2024-01-23 13:16:10 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-bce99cf5511028c7586f.js"
+    "url": "webpack-runtime-ee18c0c27c7d8f92e187.js"
   },
   {
     "url": "framework-ab5754040afb482873c1.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f2fa3e9c3892a8109a4bd274cb85155b"
+    "revision": "317ee566d7367fb7aa4b15146d9cb12a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-38a1858722f0f521dbcf.js"
